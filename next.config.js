dfbf8adbc5a73061ae65235b82f353434c3a2e80
Ediz <53665610+edizyurdakul@users.noreply.github.com>
2022-06-19 00:58:46 +0200

    Add preact on production

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+
+    return config;
+  },
+};
